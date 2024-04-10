cf7d64a05ea5c414dcd0b00c27f2cdcac19dfc3c
diksha <dd7916115@gmail.com>
2024-04-10 08:46:14 +0530

    my first

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <h1>DIKSHA's shopping cart</h1>
+          </a>
+          
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
---
+++ src/components/background.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class  Background extends React.Component {
+    
+    render() { 
+        return (
+            <img src="3bc3dbe72018ecfe76f0699405309178.jpg" class="img-fluid" alt="shop-img"></img>
+        );
+    }
+}
+ 
+export default Background;
